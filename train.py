60d3858736933c802d29b463d5713412d0d03c98
aa <wubinray@gmail.com>
2020-05-26 07:57:30 +0800

    train

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
 from model import * 
 from loss import *
 
-import os, glob 
+import os, glob, json
+
+def mean(ls):
+    return sum(ls)/(len(ls)+1e-8)
 
 def _run_epoch(dataloader, model, opt, criterion):
     
     model.train()
     
-    total_loss = 0
+    step_loss = [] 
     for idx, (video, gt_img) in enumerate(dataloader):
         b, s, c, h, w = video.shape
         video = video.cuda()
@@ -18,20 +21,19 @@ def _run_epoch(dataloader, model, opt, criterion):
         loss.backward()
         opt.step()
         
-        total_loss += loss.item() * b
-        print('\t [{}/{}] Traing Loss:{:.4f} '.format(idx+1, len(dataloader), loss.item()),
+        step_loss.append(loss.item())
+        print('\t [{}/{}] Traing Loss:{:.4f} '.format(idx+1, len(dataloader), mean(step_loss)),
              end='      \r')        
         torch.cuda.empty_cache()
     
-    avg_loss = total_loss/len(dataloader.dataset)
-    return avg_loss
+    return step_loss 
     
 def _run_val(dataloader, model, criterion):
     
     model.eval()
     
     with torch.no_grad():
-        total_loss = 0
+        step_loss = []
         for idx, (video, gt_img) in enumerate(dataloader):
             b, s, c, h, w = video.shape
             video = video.cuda()
@@ -40,14 +42,12 @@ def _run_val(dataloader, model, criterion):
             out = model(video)
             loss = criterion(out, gt_img)
             
-            total_loss += loss.item() * b
-            
-            print('\t [{}/{}] Valid Loss:{:.4f} '.format(idx+1, len(dataloader), loss.item()),
+            step_loss.append(loss.item())
+            print('\t [{}/{}] Valid Loss:{:.4f} '.format(idx+1, len(dataloader), mean(step_loss)),
                  end='      \r')
             torch.cuda.empty_cache()
             
-    avg_loss = total_loss/len(dataloader.dataset)
-    return avg_loss
+    return step_loss
 
 def train(args, train_dataloader, valid_dataloader):
     torch.manual_seed(87)
@@ -65,31 +65,35 @@ def train(args, train_dataloader, valid_dataloader):
     #criterion = Perceptual_Loss().cuda()
     criterion = Consistency_Loss().cuda() 
 
-    lr, best_loss = args.lr, None
+    train_step_loss, valid_step_loss, best_loss = [], [], None
     for epoch in range(args.from_epoch+1, args.from_epoch+args.epoch+1):
         print(f' Epoch {epoch}')
-        print('\t [Info] lr:{:.5f}'.format(lr))
+        print('\t [Info] lr:{:e}'.format(args.lr))
         
-        avg_train_loss = _run_epoch(train_dataloader, model, opt, criterion)
-        print('\t [Info] Avg Traing Loss:{:.4f} '.format(avg_train_loss))
+        loss = _run_epoch(train_dataloader, model, opt, criterion)
+        print('\t [Info] Avg Traing Loss:{:.4f} '.format(mean(loss)))
         
-        avg_valid_loss = _run_val(valid_dataloader, model, criterion)
-        print('\t [Info] Avg Valid Loss:{:.4f} '.format(avg_valid_loss))
+        loss = _run_val(valid_dataloader, model, criterion)
+        print('\t [Info] Avg Valid Loss:{:.4f} '.format(mean(loss)))
         
         ## change lr
+        avg_valid_loss = mean(loss)
         if best_loss is None or avg_valid_loss < best_loss:
             best_loss = avg_valid_loss
         else:
+            args.lr /= 4
             for param_group in opt.param_groups:
-                param_group['lr'] /= 5
-                lr += param_group['lr']
-            lr /= len(opt.param_groups)
+                param_group['lr'] /= args.lr 
         
         ## save path
         save_path = "{}/epoch_{}_loss_{:.4f}.pt".format(
                 args.model_path, epoch, avg_valid_loss)
         torch.save({'state_dict': model.state_dict()}, save_path)
         print(f'\t [Info] save weights at {save_path}') 
+       
+        ## save learning curve
+        with open(args.model_path+'/lr_curve.json', 'w') as f:
+            json.dump({'train':train_step_loss,'valid':valid_step_loss},f,indent=4)
         
         print('---------------------------------------------------')
        
